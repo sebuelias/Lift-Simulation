d6fcd257a0adf8356e691840a2d144aaec8272ac
sebuelias <sebuelias27@gmail.com>
2021-06-30 17:03:41 +0530

    seperatiing lift and key chambers

--- src/App.js
+++ src/App.js
@@ -9,104 +9,138 @@ import './App.css';
 //TODO: Animate the lift movement
 function App() {
   const [currentFloor, setCurrentFloor] = useState(1);
+  const [numOfFloors, setNumOfFloors] = useState(4);
 
   //!To be used when multiple lifts and adding up/down logic
   const [floorPressed, setFloorPressed] = useState(1);
 
   // return <Lift />;
+  let keys = '';
+  let lift = '';
+
+  // for (let i = numOfFloors; i > 0; i -= 1) {
+  //   keys += `<div className='keys'>
+  //     ${
+  //       i !== numOfFloors ? (
+  //         <button
+  //           className='button up-button'
+  //           onClick={() => {
+  //             setFloorPressed(i);
+  //             setCurrentFloor(i);
+  //           }}
+  //         >
+  //           UP
+  //         </button>
+  //       ) : null
+  //     }
+  //     ${
+  //       i !== 1 ? (
+  //         <button
+  //           className='button down-button'
+  //           onClick={() => {
+  //             setFloorPressed(i);
+  //             setCurrentFloor(i);
+  //           }}
+  //         >
+  //           DOWN
+  //         </button>
+  //       ) : null
+  //     }
+  //   </div>`;
+
+  //   lift += `${currentFloor === i ? <div className='lift-element' /> : null}`;
+  // }
+
   return (
-    <div class='container'>
-      <div className='lift-container'>
-        <div className='floor'>
-          <div className='floor-keys'>
-            <button
-              className='button down-button'
-              onClick={() => {
-                setFloorPressed(4);
-                setCurrentFloor(4);
-              }}
-            >
-              DOWN
-            </button>
-          </div>
-          <div className='lift'>
-            {currentFloor === 4 ? <div className='lift-element' /> : null}
-          </div>
-          {/* <div className='lift'>L2</div> */}
-          <div className='floor-num'>Floor no. 4</div>
+    <div className='container'>
+      <div className='keys-container vertical-floors'>
+        <div className='keys floors'>
+          <button
+            className='button down-button'
+            onClick={() => {
+              setFloorPressed(4);
+              setCurrentFloor(4);
+            }}
+          >
+            DOWN
+          </button>
         </div>
-        <div className='floor'>
-          <div className='floor-keys'>
-            <button
-              className='button up-button'
-              onClick={() => {
-                setFloorPressed(3);
-                setCurrentFloor(3);
-              }}
-            >
-              UP
-            </button>
-            <button
-              className='button down-button'
-              onClick={() => {
-                setFloorPressed(3);
-                setCurrentFloor(3);
-              }}
-            >
-              DOWN
-            </button>
-          </div>
-          <div className='lift'>
-            {currentFloor === 3 ? <div className='lift-element' /> : null}
-          </div>
-          {/* <div className='lift'>L2</div> */}
-          <div className='floor-num'>Floor no. 3</div>
+
+        <div className='keys floors'>
+          <button
+            className='button up-button'
+            onClick={() => {
+              setFloorPressed(3);
+              setCurrentFloor(3);
+            }}
+          >
+            UP
+          </button>
+          <button
+            className='button down-button'
+            onClick={() => {
+              setFloorPressed(3);
+              setCurrentFloor(3);
+            }}
+          >
+            DOWN
+          </button>
         </div>
-        <div className='floor'>
-          <div className='floor-keys'>
-            <button
-              className='button up-button'
-              onClick={() => {
-                setFloorPressed(2);
-                setCurrentFloor(2);
-              }}
-            >
-              UP
-            </button>
-            <button
-              className='button down-button'
-              onClick={() => {
-                setFloorPressed(2);
-                setCurrentFloor(2);
-              }}
-            >
-              DOWN
-            </button>
-          </div>
-          <div className='lift'>
-            {currentFloor === 2 ? <div className='lift-element' /> : null}
-          </div>
-          {/* <div className='lift'>L2</div> */}
-          <div className='floor-num'>Floor no. 2</div>
+
+        <div className='keys floors'>
+          <button
+            className='button up-button'
+            onClick={() => {
+              setFloorPressed(2);
+              setCurrentFloor(2);
+            }}
+          >
+            UP
+          </button>
+          <button
+            className='button down-button'
+            onClick={() => {
+              setFloorPressed(2);
+              setCurrentFloor(2);
+            }}
+          >
+            DOWN
+          </button>
         </div>
-        <div className='floor'>
-          <div className='floor-keys'>
-            <button
-              className='button up-button'
-              onClick={() => {
-                setFloorPressed(1);
-                setCurrentFloor(1);
-              }}
-            >
-              UP
-            </button>
-          </div>
-          <div className='lift'>
-            {currentFloor === 1 ? <div className='lift-element' /> : null}
-          </div>
-          {/* <div className='lift'>L2</div> */}
-          <div className='floor-num'>Floor no. 1</div>
+
+        <div className='keys floors'>
+          <button
+            className='button up-button'
+            onClick={() => {
+              setFloorPressed(1);
+              setCurrentFloor(1);
+            }}
+          >
+            UP
+          </button>
         </div>
+      </div>
+      <div className='lift-container vertical-floors'>
+        {currentFloor === 4 ? (
+          <div className='lift-element-active floors' />
+        ) : (
+          <div className='lift-element-inactive floors' />
+        )}
+        {currentFloor === 3 ? (
+          <div className='lift-element-active floors' />
+        ) : (
+          <div className='lift-element-inactive floors' />
+        )}
+        {currentFloor === 2 ? (
+          <div className='lift-element-active floors' />
+        ) : (
+          <div className='lift-element-inactive floors' />
+        )}
+        {currentFloor === 1 ? (
+          <div className='lift-element-active floors' />
+        ) : (
+          <div className='lift-element-inactive floors' />
+        )}
       </div>
     </div>
   );
