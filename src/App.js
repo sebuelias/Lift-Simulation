8ff10545ac3868b346c9be771dbc810226085125
sebuelias <sebuelias27@gmail.com>
2021-07-01 17:18:38 +0530

    reversed, made seperate components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// import Lift from './components/Lift';
 import React, { useState } from 'react';
+import Floor from './components/Floor';
 import './App.css';
 
 //TODO: make floors into seperate component
@@ -10,138 +10,32 @@ import './App.css';
 function App() {
   const [currentFloor, setCurrentFloor] = useState(1);
   const [numOfFloors, setNumOfFloors] = useState(4);
-
   //!To be used when multiple lifts and adding up/down logic
   const [floorPressed, setFloorPressed] = useState(1);
 
-  // return <Lift />;
-  let keys = '';
-  let lift = '';
+  const onFloorKeyPress = (floorNumber) => {
+    setFloorPressed(floorNumber);
+    setCurrentFloor(floorNumber);
+  };
 
-  // for (let i = numOfFloors; i > 0; i -= 1) {
-  //   keys += `<div className='keys'>
-  //     ${
-  //       i !== numOfFloors ? (
-  //         <button
-  //           className='button up-button'
-  //           onClick={() => {
-  //             setFloorPressed(i);
-  //             setCurrentFloor(i);
-  //           }}
-  //         >
-  //           UP
-  //         </button>
-  //       ) : null
-  //     }
-  //     ${
-  //       i !== 1 ? (
-  //         <button
-  //           className='button down-button'
-  //           onClick={() => {
-  //             setFloorPressed(i);
-  //             setCurrentFloor(i);
-  //           }}
-  //         >
-  //           DOWN
-  //         </button>
-  //       ) : null
-  //     }
-  //   </div>`;
-
-  //   lift += `${currentFloor === i ? <div className='lift-element' /> : null}`;
-  // }
+  const generateFloorsArray = (totalFloors) => {
+    console.log([...Array(totalFloors + 1).keys()].slice(1).reverse())
+    return [...Array(totalFloors + 1).keys()].slice(1).reverse();
+  };
 
   return (
     <div className='container'>
-      <div className='keys-container vertical-floors'>
-        <div className='keys floors'>
-          <button
-            className='button down-button'
-            onClick={() => {
-              setFloorPressed(4);
-              setCurrentFloor(4);
-            }}
-          >
-            DOWN
-          </button>
-        </div>
-
-        <div className='keys floors'>
-          <button
-            className='button up-button'
-            onClick={() => {
-              setFloorPressed(3);
-              setCurrentFloor(3);
-            }}
-          >
-            UP
-          </button>
-          <button
-            className='button down-button'
-            onClick={() => {
-              setFloorPressed(3);
-              setCurrentFloor(3);
-            }}
-          >
-            DOWN
-          </button>
-        </div>
-
-        <div className='keys floors'>
-          <button
-            className='button up-button'
-            onClick={() => {
-              setFloorPressed(2);
-              setCurrentFloor(2);
-            }}
-          >
-            UP
-          </button>
-          <button
-            className='button down-button'
-            onClick={() => {
-              setFloorPressed(2);
-              setCurrentFloor(2);
-            }}
-          >
-            DOWN
-          </button>
-        </div>
-
-        <div className='keys floors'>
-          <button
-            className='button up-button'
-            onClick={() => {
-              setFloorPressed(1);
-              setCurrentFloor(1);
-            }}
-          >
-            UP
-          </button>
-        </div>
-      </div>
-      <div className='lift-container vertical-floors'>
-        {currentFloor === 4 ? (
-          <div className='lift-element-active floors' />
-        ) : (
-          <div className='lift-element-inactive floors' />
-        )}
-        {currentFloor === 3 ? (
-          <div className='lift-element-active floors' />
-        ) : (
-          <div className='lift-element-inactive floors' />
-        )}
-        {currentFloor === 2 ? (
-          <div className='lift-element-active floors' />
-        ) : (
-          <div className='lift-element-inactive floors' />
-        )}
-        {currentFloor === 1 ? (
-          <div className='lift-element-active floors' />
-        ) : (
-          <div className='lift-element-inactive floors' />
-        )}
-      </div>
+      {generateFloorsArray(numOfFloors).map((floorNum) => {
+        return (
+          <Floor
+            floorNumber={floorNum}
+            totalFloors={numOfFloors}
+            onKeyPress={onFloorKeyPress}
+            currentFloor={currentFloor}
+          />
+        );
+      })}
+      {/* <div className='lift-element'></div> */}
     </div>
   );
 }
--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
 import './styles.scss';
 
-export default function Floor() {
-  const [numOfLift, setNumOfLift] = useState(2);
-
-  // const liftOnFloor = `<div className='floor-lift'>Lift</div>`
+export default function Floor(props) {
+  const { floorNumber, totalFloors, onKeyPress, currentFloor } = props;
+  // const [numOfLift, setNumOfLift] = useState(2);
 
   return (
-    <div className='floor'>
-      <div className='floor-keys'>Keys</div>
-      <div className='lift'>L1</div>
-      <div className='lift'>L2</div>
+    <div className='floor' key={floorNumber}>
+      <div className='floor-keys'>
+        {floorNumber !== totalFloors ? (
+          <button
+            className='button up-button'
+            onClick={() => {
+              onKeyPress(floorNumber);
+            }}
+          >
+            UP
+          </button>
+        ) : null}
+        {floorNumber !== 0 ? (
+          <button
+            className='button down-button'
+            onClick={() => {
+              onKeyPress(floorNumber);
+            }}
+          >
+            DOWN
+          </button>
+        ) : null}
+      </div>
+      <div className='lift'>
+        {currentFloor === floorNumber ? (
+          <div className='lift-element'></div>
+        ) : null}
+      </div>
+      <div className='floor-num'>Floor Number: {floorNumber}</div>
     </div>
   );
 }
